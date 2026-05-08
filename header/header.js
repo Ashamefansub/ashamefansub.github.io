add2c07fb5f15c2ab8aa2c7b5dab57f21c0b60e2
Ashamefansub <ashamefansub@gmail.com>
2026-05-08 18:23:04 +0700

    Update 8 Mei 2026, Fitur JS Header

---
+++ header/header.js
@@ -0,0 +1,6 @@
+console.log ("Tes JS")
+fetch("../header/header.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("header").innerHTML = data;
+    });
